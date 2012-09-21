c4beabea02d9642695ff7bf38d09188eac204fd3
jdegroot <jdegroot@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-21 13:09:41 +0000

    Modified LinkViewHelper; only add action to link if news item is not of type external or internal URL
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/roq_newsevent/branches/2.0.1.1-externalurls@66262 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/LinkViewHelper.php
+++ Classes/ViewHelpers/LinkViewHelper.php
@@ -8,6 +8,10 @@
  * @description:    ViewHelper to render proper links for event detail view
  */
 
+define('NEWS_TYPE_DEFAULT', 0);
+define('NEWS_TYPE_URL_INTERNAL', 1);
+define('NEWS_TYPE_URL_EXTERNAL', 2);
+
 class Tx_RoqNewsevent_ViewHelpers_LinkViewHelper extends Tx_News_ViewHelpers_LinkViewHelper {
 
     /**
@@ -21,14 +25,14 @@ class Tx_RoqNewsevent_ViewHelpers_LinkViewHelper extends Tx_News_ViewHelpers_Lin
      * @return string url
      */
     public function render(Tx_News_Domain_Model_News $newsItem, array $settings = array(), $hsc = FALSE, $configuration = array(), $action = NULL) {
-        if($action !== NULL) {
+        if(($newsItem->getType() == NEWS_TYPE_DEFAULT) && ($action !== NULL)) {
             $configuration['additionalParams'] .= '&tx_news_pi1[action]=' . $action;
         }
 
         $link = parent::render($newsItem, $settings, $hsc, $configuration);
 
         // ignore the getDynamicGetVarsManipulation settings, which can cause the action to be added hardcoded (which is only applicable for news only items)
-        if(stristr($link, urlencode('tx_news_pi1[action]') . '=detail') !== FALSE) {
+        if(($newsItem->getType() == NEWS_TYPE_DEFAULT) && stristr($link, urlencode('tx_news_pi1[action]') . '=detail') !== FALSE) {
             $link = str_replace(urlencode('tx_news_pi1[action]') . '=detail', urlencode('tx_news_pi1[action]') . '=' . $action, $link);
         }
 
