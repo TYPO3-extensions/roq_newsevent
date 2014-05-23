2a348d79002633db2ddab0c1d78aa55aa209b9b2
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2014-05-23 22:15:36 +0200

    [FEATURE] Display FlexForm settings in the News CE
    The FlexForm settings of a News/Events plugin should be displayed
    like in EXT:news.
    
    Resolves: #59686
    Releases: 3.0
    Change-Id: I68f092f902e444819afb51225186f98402924a55
    Reviewed-on: http://review.typo3.org/30902
    Reviewed-by: Patrick Wiggelman <patrickwiggelman@roquin.nl>
    Tested-by: Patrick Wiggelman <patrickwiggelman@roquin.nl>

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,10 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+// Page module hook
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['news_pi1'][$_EXTKEY] =
+	'EXT:' . $_EXTKEY . '/Classes/Hooks/CmsLayout.php:Tx_Roqnewsevent_Hooks_CmsLayout->getExtensionSummary';
+
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['switchableControllerActions']['newItems']['--div--'] = 'Events';
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['switchableControllerActions']['newItems']['News->eventList;News->eventDetail'] = 'List view';
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['switchableControllerActions']['newItems']['News->eventDetail'] = 'Detail view';
